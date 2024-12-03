3e6596c135fc37cc832d0dffb700fe00bd40ac83
PJ1256 <parkerjesco@gmail.com>
2024-12-03 18:22:48 -0500

    Login/Logout connected to frontend

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,7 +4,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; 
 
-function Navbar() {
+function Navbar({handleLogout}) {
+  console.log("In navbar: ");
+  console.log(JSON.parse(localStorage.getItem("userLogged")));
   return (
     <header className="navbar">
       <h1>Carbon Footprint Tracker</h1>
@@ -14,6 +16,13 @@ function Navbar() {
           <li><Link to="/signup">Sign Up</Link></li>
           <li><Link to="/login">Log In</Link></li>
           <li><Link to="/Dashboard">Dashboard</Link></li>
+          <li>{JSON.parse(localStorage.getItem("userLogged"))!==null ? (
+            console.log("user not null"),
+            <button onClick={handleLogout}>Logout</button>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+          </li>
         </ul>
       </nav>
     </header>
